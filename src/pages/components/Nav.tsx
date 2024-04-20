be8e69394887e8510fdff5a000dc65cf230d8677
tine <99731290+tinemuz@users.noreply.github.com>
2024-04-20 19:00:23 +0100

    styling when menu is toggled

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,21 +1,30 @@
-//import avatar from "/avatar.png"
-import { motion } from 'framer-motion'
-import { useState } from "react"
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Nav() {
-    const [toggled, setToggled] = useState(false)
-    return (
-        <header className='flex flex-wrap w-full py-4 px-4'>
-            <nav className='w-full mx-auto flex items-center justify-between'>
-                <span className='flex-none w-[25px] h-[25px] bg-stone-800 rounded-full'></span>
-                <div className='flex flex-row items-center gap-3 mt-0 ps-5'>
-                    <span className='right-20 w-[16px] h-[16px] bg-blue-700 rounded-full'></span>
-                    <span className='right-12 w-[16px] h-[16px] bg-red-500 rounded-full'></span>
-                    <span className='right-0 w-[40px] h-[16px] bg-stone-800 rounded-full'></span>
-                </div>
-
-            </nav>
-        </header>
-
-    )
+  const [toggled, setToggled] = useState(false);
+  return (
+    <header className="flex flex-wrap w-full py-5 px-6">
+      <nav className="w-full flex items-center justify-between">
+        <span className="flex-none size-8 bg-stone-800 rounded-full"></span>
+        <div
+          onClick={() => setToggled((prevToggle) => !prevToggle)}
+          className={`flex flex-row gap-[5px] pl-2 py-2 cursor-pointer sm:gap-3  sm:pointer-events-none z-50 ${toggled && 'gap-3'}`}
+        >
+          <span className={`size-[6px] sm:size-[16px] bg-blue-700  rounded-full ${toggled && 'size-[16px]'}`}></span>
+          <span className={`size-[6px] sm:size-[16px] bg-red-500  rounded-full ${toggled && 'size-[16px]'}`}></span>
+          <span className={`size-[6px] sm:w-[40px] sm:h-[16px] bg-stone-800  rounded-full ${toggled && 'w-[40px] h-[16px]'}`}></span>
+        </div>
+        {toggled && (
+          <div className="fixed flex bg-stone-200 bottom-0 left-0 w-full h-screen items-center justify-center">
+            <div className="flex flex-col gap-12">
+              <a href="/">Home</a>
+              <a href="/writing">Writing</a>
+              <a href="/projects">Projects</a>
+            </div>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 }
