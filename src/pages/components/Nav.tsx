5c3bf0ed08482f88d5463c7e8b3787a5499cefc5
tine <99731290+tinemuz@users.noreply.github.com>
2024-04-20 22:29:10 +0100

    nav

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -14,8 +14,8 @@ export default function Nav() {
         {!matches && (
           <div
             onClick={() => setToggled((prevToggle) => !prevToggle)}
-            className={`flex flex-row gap-[5px] pl-2 py-2 cursor-pointer sm:pointer-events-none z-50 ${
-              toggled && "gap-5"
+            className={`flex flex-row pl-2 py-2 cursor-pointer sm:pointer-events-none z-50 ${
+              toggled ? "gap-5": "gap-[5px]"
             }`}
           >
             <motion.span
