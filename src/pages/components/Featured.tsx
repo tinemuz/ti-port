ddce2c0294d0e64b4aef59a1b214cbb809a8bb55
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-16 16:40:22 +0100

    clean up

---
+++ src/pages/components/Featured.tsx
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useMediaQuery } from "../../util/useMediaQuery";
+
+export default function Featured() {
+  return (
+    <div className="">
+      <div className="">
+        {/* Section One */}
+        <div className="grid h-5 grid-cols-3 border border-mgray">
+          <div className="col-span-2"></div>
+          <div className="border-l border-l-mgray"></div>
+        </div>
+        <div className="aspect-3/1 grid grid-cols-3 border border-b-0 border-t-0 border-mgray">
+          <div className="col-span-2"></div>
+          <div className="col-span-1 border-l border-l-mgray"></div>
+        </div>
+        {/* Section Two */}
+        <div className="grid h-5 grid-cols-3 border border-mgray">
+          <div className=""></div>
+          <div className="border-l border-l-mgray"></div>
+          <div className="border-l border-l-mgray"></div>
+        </div>
+        <div className="aspect-3/1 grid grid-cols-3 border border-b-0 border-t-0 border-mgray">
+          <div className=""></div>
+          <div className="border-l border-l-mgray"></div>
+          <div className="border-l border-l-mgray"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
