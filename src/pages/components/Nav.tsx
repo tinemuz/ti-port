51a5770d91c10116abda4a9fea35405938d912f0
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-17 09:15:58 +0100

    nav bar

---
+++ src/pages/components/Nav.tsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { useMediaQuery } from "../../util/useMediaQuery";
+
+export default function Nav() {
+  return (
+    <header className="flex flex-grow flex-col items-center justify-center z-[100] text-sm text-mtext">
+
+          <div className="w-full md:max-w-[812px]">
+            <div className="flex justify-end gap-12 pt-60">
+              <a href="/">Featured</a>
+              <a href="/writing">Writing</a>
+              <a href="/projects">Side Quests</a>
+            </div>
+          </div>
+
+    </header>
+  );
+}
