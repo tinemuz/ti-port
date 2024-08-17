e74e338199efe1db2988c44c4331954bffe15aab
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-17 10:27:52 +0100

    nav

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,15 +1,17 @@
 export default function Nav() {
   return (
-    <header className="flex flex-grow flex-col items-center justify-center z-[100] text-sm text-mtext">
+    <nav className="flex h-[240px] flex-grow flex-col items-center justify-center md:text-sm text-mtext pl-6 pr-6">
+      <div className="h-full flex-grow"></div>
 
-          <div className="w-full md:max-w-[812px]">
-            <div className="flex justify-end gap-12 pt-60">
-              <a href="/">Featured</a>
-              <a href="/writing">Writing</a>
-              <a href="/projects">Side Quests</a>
-            </div>
-          </div>
+      <div className="w-full flex flex-row justify-between md:max-w-[812px] items-center">
+        <div className="size-5 rounded-full bg-mtext"></div>
 
-    </header>
+        <div className="flex gap-6">
+          <a href="/">Featured</a>
+          <a href="/writing">Writing</a>
+          <a href="/projects">Side Quests</a>
+        </div>
+      </div>
+    </nav>
   );
 }
