e736da7e57e000e4735b6c0bf9ba03894282e35f
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-20 20:23:51 +0100

    footer base

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className=" flex h-[220px] justify-center pb-4 pl-6 pr-6 pt-12">
+    <footer className=" flex h-[300px] md:h-[220px] justify-center pb-4 pt-12">
       <div className=" flex w-full flex-col border-t border-t-mgray pt-6 text-xs md:max-w-[812px]">
-        <div className="grid h-full w-full grid-cols-3 gap-4">
+        <div className="grid h-full w-full grid-cols-2 md:grid-cols-3 gap-y-8">
           <div className="flex flex-col items-start space-y-1">
             <a href="mailto:tine@muzunza.com">tine@muzunza.com</a>
             <a href="https://www.linkedin.com">Linkedin</a>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
   return (
-    <nav className="flex h-[240px] flex-grow flex-col items-center justify-center md:text-sm text-mtext pl-6 pr-6">
+    <nav className="flex h-[240px] flex-grow flex-col items-center justify-center md:text-sm text-mtext">
       <div className="h-full flex-grow"></div>
 
       <div className="w-full flex flex-row justify-between md:max-w-[812px] items-center">
