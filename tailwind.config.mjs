a0896dc9a1ef196c6adbac50c0ba6215f40f3882
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-21 01:44:27 +0100

    clean up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {	
-
-		extend: {
-			fontFamily: {
-				sans: ['DM Mono', ...defaultTheme.fontFamily.mono],
-			},
-			aspectRatio: {
-				'2/1': '2 / 1',
-				'3/1': '3 / 1',
-			},
-		},
-		colors: {
-			'mgray': '#A6A6A6',
-			'back': '#BEBEBE',
-			'mtext': '#222222',
-			'mred': '#F14E3A',
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["DM Mono", ...defaultTheme.fontFamily.mono],
+      },
+      aspectRatio: {
+        "2/1": "2 / 1",
+        "3/1": "3 / 1",
+      },
+    },
+    colors: {
+      mgray: "#D8D8D8",
+      back: "#EAEAEA",
+      mtext: "#242424",
+      mred: "#F14E3A",
+    },
+  },
+  plugins: [],
+};
