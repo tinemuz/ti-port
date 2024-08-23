e9acb7a7c87c1581e5823e270b2014d268fcf25a
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-23 15:20:29 +0100

    Merge branch 'origin/new'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import TimeDisplay from "./TimeDisplay";
+import { BSTTimeDisplay, LocalTimeDisplay } from "./TimeDisplay";
 
 export default function Footer() {
   return (
-    <footer className=" flex h-[270px] justify-center pb-4 pt-12 md:h-[220px]">
-      <div className=" flex w-full flex-col border-t border-t-mgray pt-6 text-xs md:max-w-[812px]">
+    <footer className=" flex h-[270px] justify-center pb-4 pt-10 md:h-[220px]">
+      <div className=" flex w-full flex-col border-t border-t-mgray pt-10 text-xs md:max-w-[812px]">
         <div className="grid w-full grid-cols-2 gap-y-5 md:grid-cols-3">
-          <div className="flex flex-col items-start space-y-1">
+          <div className="flex flex-col items-start space-y-2">
             <a href="mailto:tine@muzunza.com">tine@muzunza.com</a>
             <a
               href="https://www.linkedin.com/in/tinetariro-muzunzandare"
@@ -17,17 +17,17 @@ export default function Footer() {
               Instagram
             </a>
           </div>
-          <div className="flex flex-col items-start space-y-1">
+          <div className="flex flex-col items-start space-y-2">
             <a href="/">Featured</a>
             <a href="/writing">Writing</a>
             <a href="/tags">Tags</a>
           </div>
-          <div className="flex flex-col items-start space-y-1">
+          <div className="flex flex-col items-start space-y-2">
             <a href="https://github.com/tinemuz" target="_blank">
               GitHub
             </a>
-            <div>UK . 2024</div>
-            <TimeDisplay />
+            <BSTTimeDisplay/>
+            <LocalTimeDisplay />
           </div>
         </div>
         <div className="h-full"></div>
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -6,6 +6,8 @@ const formatTime = (date: Date, timeZone: string) => {
   return date.toLocaleTimeString(undefined, {
     hour: "2-digit",
     minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
     timeZone,
   });
 };
