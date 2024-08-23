3dc4d7186c9de11bf226b05e47b300727024128d
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-23 13:35:08 +0100

    Squashed commit of the following:
    commit 9527b8d8d36a7b95dc2a042420d383812abedf69
    Author: tine <99731290+tinemuz@users.noreply.github.com>
    Date:   Fri Aug 23 13:34:15 2024 +0100
    
        clean up and optimisations
    
    commit ecc3cbbe7325611fa82b644b4cc8d960b3efaffe
    Author: tine <99731290+tinemuz@users.noreply.github.com>
    Date:   Fri Aug 23 13:12:03 2024 +0100
    
        clean
    
    commit ea2e08a001ec2c9f5851c04856713f310e1ef6a4
    Merge: d838925 d8f4311
    Author: tine <99731290+tinemuz@users.noreply.github.com>
    Date:   Fri Aug 23 13:11:14 2024 +0100
    
        clean

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import compress from "astro-compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  site: "https://tine.muzunza.com",
+  integrations: [react(), tailwind(), mdx(), sitemap(), compress()],
 });
