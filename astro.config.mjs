4329ab140299e8f5a45b93acff5b9d343f2ed717
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-25 17:32:10 +0100

    theme support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tine.muzunza.com",
-  integrations: [react(), tailwind(), mdx(), sitemap(), ViteImageOptimizer(), compress()],
+  integrations: [react(), tailwind(), mdx(), sitemap(), compress()],
   
 
   
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme";
 
+function withOpacity(variableName) {
+  return ({opacityValue}) => {
+    if (opacityValue != undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgba(var(${variableName}))`;
+  }
+}
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -11,6 +20,28 @@ export default {
       aspectRatio: {
         "2/1": "2 / 1",
         "3/1": "3 / 1",
+      },
+      textColor: {
+        skin: {
+          base: withOpacity('--color-text-base'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          background: withOpacity('--color-background'),
+          acent: withOpacity('--color-fill-acent'),
+          border: withOpacity('--color-fill-border'),
+        },
+      },
+      borderColor: {
+        skin: {
+          border: withOpacity('--color-fill-border'),
+        },
+      },
+      fill: {
+        skin: {
+          acent: withOpacity('--color-fill-acent'),
+        },
       },
     },
     colors: {
