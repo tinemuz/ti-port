5dad65cf29b5bc2e7d0551ce049a4abb3e0494b6
tine <99731290+tinemuz@users.noreply.github.com>
2024-08-30 16:15:03 +0100

    scaling

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -2,7 +2,7 @@ function formatDate(date: Date): string {
   return new Date(date).toISOString().split("T")[0];
 }
 
-function truncateTitle(str: string, length: number = 60): string {
+function truncateTitle(str: string, length: number = 24): string {
   if (str.length > length) {
     return str.slice(0, length) + "...";
   }
