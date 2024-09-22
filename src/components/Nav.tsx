1d78b78dd9be5f4fa4b086e1b656a3f260b907ba
tine <99731290+tinemuz@users.noreply.github.com>
2024-09-23 00:11:42 +0100

    clean up

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,6 @@
 import ThemeToggle from "./ThemeToggle";
 
 const Nav = () => {
-  const currentPath = window.location.pathname;
-  
   return (
     <nav className="flex h-[240px] flex-grow flex-col items-center justify-center text-skin-text sm:text-sm">
       <div className="h-full flex-grow"></div>
@@ -10,22 +8,13 @@ const Nav = () => {
       <div className="flex w-full flex-row items-center justify-between sm:max-w-[812px]">
         <ThemeToggle />
         <div className="flex gap-8">
-          <a
-            href="/"
-            className={`hover:text-skin-text-sub ${currentPath === "/" ? "font-bold" : ""}`}
-          >
+          <a href="/" className="hover:text-skin-text-sub">
             Featured
           </a>
-          <a
-            href="/writing"
-            className={`hover:text-skin-text-sub ${currentPath === "/writing" ? "font-bold" : ""}`}
-          >
+          <a href="/writing" className="hover:text-skin-text-sub">
             Writing
           </a>
-          <a
-            href="/tags"
-            className={`hover:text-skin-text-sub ${currentPath === "/tags" ? "font-bold" : ""}`}
-          >
+          <a href="/tags" className="hover:text-skin-text-sub">
             Tags
           </a>
         </div>
