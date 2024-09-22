7afa71ddbca0daeebb77b46f9daf198f91961917
tine <99731290+tinemuz@users.noreply.github.com>
2024-09-23 00:02:13 +0100

    bold text

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,34 @@
 import ThemeToggle from "./ThemeToggle";
-import NavLink from "./NavLink";
 
 const Nav = () => {
+  const currentPath = window.location.pathname;
+  
   return (
     <nav className="flex h-[240px] flex-grow flex-col items-center justify-center text-skin-text sm:text-sm">
       <div className="h-full flex-grow"></div>
 
       <div className="flex w-full flex-row items-center justify-between sm:max-w-[812px]">
         <ThemeToggle />
-        <NavLink />
+        <div className="flex gap-8">
+          <a
+            href="/"
+            className={`hover:text-skin-text-sub ${currentPath === "/" ? "font-bold" : ""}`}
+          >
+            Featured
+          </a>
+          <a
+            href="/writing"
+            className={`hover:text-skin-text-sub ${currentPath === "/writing" ? "font-bold" : ""}`}
+          >
+            Writing
+          </a>
+          <a
+            href="/tags"
+            className={`hover:text-skin-text-sub ${currentPath === "/tags" ? "font-bold" : ""}`}
+          >
+            Tags
+          </a>
+        </div>
       </div>
     </nav>
   );
