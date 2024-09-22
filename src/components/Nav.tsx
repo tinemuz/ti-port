b2b810ae70d04ec745b6b97500de2df139b59078
tine <99731290+tinemuz@users.noreply.github.com>
2024-09-23 00:47:50 +0100

    cleaner way for the bold text

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,6 @@ import { useState, type SetStateAction } from "react";
 import ThemeToggle from "./ThemeToggle";
 
 const Nav = () => {
-  const [activeLink, setActiveLink] = useState(window.location.pathname);
-
-  const handleClick = (link: SetStateAction<string>) => {
-    setActiveLink(link);
-  };
 
   return (
     <nav className="flex h-[240px] flex-grow flex-col items-center justify-center text-skin-text sm:text-sm">
@@ -17,22 +12,19 @@ const Nav = () => {
         <div className="flex gap-8">
           <a
             href="/"
-            onClick={() => handleClick("/")}
-            className={`hover:text-skin-text-sub ${activeLink === "/" ? "font-bold" : ""}`}
+            className={`hover:text-skin-text-sub ${window.location.pathname === "/" ? "font-bold" : ""}`}
           >
             Featured
           </a>
           <a
             href="/writing"
-            onClick={() => handleClick("/writing")}
-            className={`hover:text-skin-text-sub ${activeLink === "/writing" ? "font-bold" : ""}`}
+            className={`hover:text-skin-text-sub ${window.location.pathname === "/writing" ? "font-bold" : ""}`}
           >
             Writing
           </a>
           <a
             href="/tags"
-            onClick={() => handleClick("/tags")}
-            className={`hover:text-skin-text-sub ${activeLink === "/tags" ? "font-bold" : ""}`}
+            className={`hover:text-skin-text-sub ${window.location.pathname === "/tags" ? "font-bold" : ""}`}
           >
             Tags
           </a>
