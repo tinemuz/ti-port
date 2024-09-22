866d4d7722b64f8ad3a36fde5d69ecf280d36521
tine <99731290+tinemuz@users.noreply.github.com>
2024-09-23 00:33:44 +0100

    bold on click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,13 @@
+import { useState, type SetStateAction } from "react";
 import ThemeToggle from "./ThemeToggle";
 
 const Nav = () => {
+  const [activeLink, setActiveLink] = useState(window.location.pathname);
+
+  const handleClick = (link: SetStateAction<string>) => {
+    setActiveLink(link);
+  };
+
   return (
     <nav className="flex h-[240px] flex-grow flex-col items-center justify-center text-skin-text sm:text-sm">
       <div className="h-full flex-grow"></div>
@@ -8,13 +15,25 @@ const Nav = () => {
       <div className="flex w-full flex-row items-center justify-between sm:max-w-[812px]">
         <ThemeToggle />
         <div className="flex gap-8">
-          <a href="/" className="hover:text-skin-text-sub">
+          <a
+            href="/"
+            onClick={() => handleClick("/")}
+            className={`hover:text-skin-text-sub ${activeLink === "/" ? "font-bold" : ""}`}
+          >
             Featured
           </a>
-          <a href="/writing" className="hover:text-skin-text-sub">
+          <a
+            href="/writing"
+            onClick={() => handleClick("/writing")}
+            className={`hover:text-skin-text-sub ${activeLink === "/writing" ? "font-bold" : ""}`}
+          >
             Writing
           </a>
-          <a href="/tags" className="hover:text-skin-text-sub">
+          <a
+            href="/tags"
+            onClick={() => handleClick("/tags")}
+            className={`hover:text-skin-text-sub ${activeLink === "/tags" ? "font-bold" : ""}`}
+          >
             Tags
           </a>
         </div>
