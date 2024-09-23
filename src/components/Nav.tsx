fd5ad6d6d00ebe92403f1c5b6f05c832277e16b7
tine <99731290+tinemuz@users.noreply.github.com>
2024-09-23 01:15:14 +0100

    opacinng on inactive links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   const activeLink = window.location.pathname;
 
   return (
-    <nav className="flex h-[240px] flex-grow flex-col items-center justify-center text-skin-text sm:text-sm">
+    <nav className="flex h-[240px] flex-grow flex-col items-center justify-center text-skin-text/65 sm:text-sm">
       <div className="h-full flex-grow"></div>
 
       <div className="flex w-full flex-row items-center justify-between sm:max-w-[812px]">
@@ -12,19 +12,19 @@ const Nav = () => {
         <div className="flex gap-8">
           <a
             href="/"
-            className={`hover:text-skin-text-sub ${activeLink === "/" ? "font-bold" : ""}`}
+            className={`hover:text-skin-text-sub ${activeLink === "/" ? "font-bold text-skin-text/100" : ""}`}
           >
             Featured
           </a>
           <a
             href="/writing"
-            className={`hover:text-skin-text-sub ${activeLink.includes("/writing") ? "font-bold" : ""}`}
+            className={`hover:text-skin-text-sub ${activeLink.includes("/writing") ? "font-bold text-skin-text/100" : ""}`}
           >
             Writing
           </a>
           <a
             href="/tags"
-            className={`hover:text-skin-text-sub ${activeLink.includes("/tags") ? "font-bold" : ""}`}
+            className={`hover:text-skin-text-sub ${activeLink.includes("/tags") ? "font-bold text-skin-text/100" : ""}`}
           >
             Tags
           </a>
