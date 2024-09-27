98dc8d83963b274166de38d9c7004ca18fccae2a
tine <99731290+tinemuz@users.noreply.github.com>
2024-09-27 06:54:21 +0100

    minimal words grid

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -5,6 +5,26 @@ function formatDate(date: Date): string {
   return `${year}.${month}.${day}`;
 }
 
+function formatDateMonthYear(date: Date): string {
+  const year = date.getFullYear();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = monthNames[date.getMonth()];
+  return `${month} • ${year}`;
+}
+
 function truncateTitle(str: string, length: number = 34): string {
   if (str.length > length) {
     return str.slice(0, length) + "...";
@@ -12,4 +32,4 @@ function truncateTitle(str: string, length: number = 34): string {
   return str;
 }
 
-export { formatDate, truncateTitle };
+export { formatDate, truncateTitle, formatDateMonthYear };
